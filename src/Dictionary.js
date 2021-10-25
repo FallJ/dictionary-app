38cb99477070174a571cfc2beb30136f5da9b4ed
FallJ <89920600+FallJ@users.noreply.github.com>
2021-10-25 15:09:31 -0400

    removed alert

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,38 +1,30 @@
-import React, { useState }from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 
-
-
 export default function Dictionary() {
-const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("");
 
-function handleResponse(response) {
-console.log(response.data[0]);
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
+  function search(event) {
+    event.preventDefault();
 
-}
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function search(event) {
-        event.preventDefault();
-
-
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-    }
-
-function handleKeywordChange(event) {
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
 
-}
-
-
-
-https: return (
-  <div className="dictionary">
-    <form onSubmit={search}>
-      <input type="search" onChange={handleKeywordChange} />
-    </form>
-  </div>
-);
+  https: return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
