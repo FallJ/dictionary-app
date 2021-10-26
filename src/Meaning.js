0944b857ff6c57533ed5c265aa3d09945df86918
FallJ <89920600+FallJ@users.noreply.github.com>
2021-10-26 10:36:45 -0400

    Added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,15 +11,16 @@ return (
 {props.meaning.definitions.map(function(definition, index) {
 
 return (
-    <div key={index}> 
-    <p><strong>Definition:</strong> {definition.definition}
-    <br />
-    <strong>Example:</strong> 
-    <em>
-    {definition.example}</em> </p>
-    <br />
-    <Synonyms synonyms={definition.synonyms} />
-    </div>
+  <div key={index}>
+    <p>
+      <strong>Definition:</strong> {definition.definition}
+      <br />
+      <strong>Example:</strong>
+      <em>{definition.example}</em>
+      <br />
+      <Synonyms synonyms={definition.synonyms} />
+    </p>
+  </div>
 );
 })}
    
