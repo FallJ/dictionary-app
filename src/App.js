25d9bbeb26bb81ae767e89a62e2f3c8302f5dab6
FallJ <89920600+FallJ@users.noreply.github.com>
2021-10-26 15:16:18 -0400

    Added photos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
-import Dictionary from "./Dictionary"
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header"></header>
-     <main>
-       <Dictionary defaultKeyword="phantom" />
-     </main>
-     <footer className="App-footer">Coded by Julie Fallan</footer>
+        <main>
+          <Dictionary defaultKeyword="phantom" />
+        </main>
+        <footer className="App-footer">
+          <a
+            href="https://github.com/FallJ/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Coded
+          </a>{" "}
+          by Julie Fallan
+        </footer>
       </div>
     </div>
   );
 }
-
-
